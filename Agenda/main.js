152e30d8ce8b50954e996e332bf50c759bddfaf7
Matheus Souza <matheus.souza@escolaweb.com.br>
2021-02-17 01:39:28 -0300

    Conexão alternativa

--- Agenda/main.js
+++ Agenda/main.js
@@ -1,27 +1,16 @@
-var mysql = require('mysql2')
 
-const conn = mysql.createConnection({
+
+async function conectar(){
+    var mysql = require('mysql2')
+    const conn = await mysql.createConnection({
     host: 'localhost',
     database: 'pessoa',
     user: 'root',
     password: ''
-})
+})}
 
-conn.connect(function(error){
-    if(error){
-        throw error
-    } else{
-        console.log('Foi')
-    }
-})
-
-conn.query('select*from pessoa', function(error, results, field){
-    if (error)
-    throw error
-
-    results.forEach(result => {
-        console.log(result)
-    })
-})
-
-conn.end()
+async function select(){
+    const conn = await conectar()
+    const [rows] = await conn.query('select * from pessoa')
+    return rows
+}
